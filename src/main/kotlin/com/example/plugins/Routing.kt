ecdbee3bb85e04160a8568b9dfe48ba8ad3793c7
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-04-24 17:37:30 +0700

    finishing http api

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -4,9 +4,8 @@ import com.example.routes.customerRouting
 import com.example.routes.getOrderRoute
 import com.example.routes.listOrderRoute
 import com.example.routes.totalizeOrderRoute
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
